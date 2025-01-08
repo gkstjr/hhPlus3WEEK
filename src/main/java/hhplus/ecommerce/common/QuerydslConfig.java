88819e5285d0a9e23b18d3ac9e5f6f8de87bf6f8
gkstjr <gkstjr8332@naver.com>
2025-01-08 14:04:11 +0900

    Chore : QueryDsl 설정

--- src/main/java/hhplus/ecommerce/common/QuerydslConfig.java
+++ src/main/java/hhplus/ecommerce/common/QuerydslConfig.java
@@ -3,16 +3,17 @@ package hhplus.ecommerce.common;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@RequiredArgsConstructor
 public class QuerydslConfig {
-    @PersistenceContext
-    private EntityManager em;
+    private final EntityManager entityManager;
 
     @Bean
-    public JPAQueryFactory querydsl() {
-        return new JPAQueryFactory(em);
+    public JPAQueryFactory jpaQueryFactory(){
+        return new JPAQueryFactory(entityManager);
     }
 }
